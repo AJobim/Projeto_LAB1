934dd9aff04d24a3e0f596ed132c759b2d4ec8ac
Jobim <amoraesjobim@gmail.com>
2025-05-28 09:54:46 -0300

    Created Endereco and its constructor

---
+++ etapa1/Endereco.java
@@ -0,0 +1,20 @@
+package etapa1;
+public class Endereco {
+    private String nomeDaRua;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String cep;
+    private String numero;
+    private String complemento;
+    
+    public Endereco(String nomeDaRua, String cidade, String estado, String pais, String cep, String numero, String complemento){
+        this.nomeDaRua = nomeDaRua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+}
